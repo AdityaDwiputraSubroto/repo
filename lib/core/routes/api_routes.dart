add8dffb9c6eef9b05e02554c95e5254a29263c9
handiism <mail.handira@gmail.com>
2023-01-14 19:28:19 +0700

    fix: sharedprefs

--- lib/core/routes/api_routes.dart
+++ lib/core/routes/api_routes.dart
@@ -1,5 +1,5 @@
 abstract class ApiRoutesRepo {
-  static const String baseUrl = 'http://10.0.2.2:3000';
+  static const String baseUrl = 'http://34.101.216.127:3000';
   static const String login = '/user/login';
   static const String forgot = '/password-reset';
   static const String register = '/user/register';
